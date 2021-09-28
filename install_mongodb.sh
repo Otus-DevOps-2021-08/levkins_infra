8a82bd2e0e41edef677f8f7ec5fa84aa2cb170d4
levkins <levkins@bk.ru>
2021-09-28 21:33:47 +0300

    Fix install_mongodb

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-apt update
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+apt update
 apt install -y mongodb-org mongodb-org-server
 mongo --version || exit
 systemctl enable mongod.service
