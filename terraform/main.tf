a535ffcefd807ca1379edcdb8e9afa80f6121c0c
levkins <levkins@bk.ru>
2021-10-07 16:21:32 +0300

    Fix provider in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.64.1"
-    }
-  }
-}
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
